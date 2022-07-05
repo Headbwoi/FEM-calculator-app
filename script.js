e296947a8322a17dde645a497e64591b1fad9845
headbwoi <tochukwujohn24@gmail.com>
2022-07-05 16:42:27 +0100

    finished the javaScriptI

--- script.js
+++ script.js
@@ -1,5 +1,129 @@
+
+class Calculator{
+    constructor(previousOperandText, currentOperandText){
+        this.previousOperandText = previousOperandText
+        this.currentOperandText = currentOperandText
+        this.resetValues()
+    }
+
+    resetValues(){
+        this.currentOperand = ""
+        this.previousOperand = ""
+        this.operation = undefined
+    }
+
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    appendNumber(number){
+        if(number === "." && this.currentOperand.includes(".")) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation){
+        if(this.operation === "")return
+        if(this.operation !== ""){
+            this.compute()
+        }
+
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ""
+    }
+
+    compute(){
+        let values
+        const previous = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+
+        if(isNaN(previous) || isNaN(current)) return
+
+        switch (this.operation) {
+            case '+':
+                values = previous + current 
+                break;
+            case '-':
+                values = previous - current 
+                break;
+            case 'x':
+                values = previous * current 
+                break;
+            case '/':
+                values = previous / current 
+                break;
+            default:
+                return
+        }
+        this.currentOperand = values
+        this.operation = undefined
+        this.previousOperand = ""
+    }
+
+    formatNumber(number){
+        const stringNumber = number.toString()
+        const integerNumber = parseFloat(stringNumber.split(".")[0])
+        const decimalNumber = stringNumber.split(".")[1]
+        let displayInteger
+        if (isNaN(integerNumber)) {
+            displayInteger = ""
+        }else{
+            displayInteger = integerNumber.toLocaleString("en", { maximumFractionDigits: 0 })
+        }
+
+        if(decimalNumber != null){
+            return `${displayInteger}.${decimalNumber}`
+        }else{
+            return displayInteger
+        }
+    }
+
+    updateDisplay(){
+        this.currentOperandText.innerText = this.formatNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandText.innerText = 
+            `${this.formatNumber(this.previousOperand)} ${this.operation}`
+        }else{
+            this.previousOperandText.innerText = ""
+        }
+    }
+}
+
 const previousOperandText = document.querySelector("[data-previous]")
 const currentOperandText = document.querySelector("[data-current]")
+const operationButtons = document.querySelectorAll("[data-operator]")
+const numberButtons = document.querySelectorAll("[data-number]")
+const deleteBtn = document.querySelector("[data-delete]")
+const resetBtn = document.querySelector("[data-clearAll]")
+const equalsBtn = document.querySelector("[data-equals]")
 
 
-console.log(currentOperandText);
+const calculator = new Calculator(previousOperandText, currentOperandText)
+
+
+numberButtons.forEach(number => {
+    number.addEventListener("click", () => {
+        let numValue = number.innerText
+        calculator.appendNumber(numValue)
+        calculator.updateDisplay()
+    })
+})
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+resetBtn.addEventListener("click", () => {
+    calculator.resetValues()
+    calculator.updateDisplay()
+})
+deleteBtn.addEventListener("click", () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+equalsBtn.addEventListener("click", () => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
